ea336525064879945c0e4c0290061b32cb0fce7f
jdhofmann <justindhofmann@gmail.com>
2021-02-02 12:05:03 -0500

    post list displays

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Layout from "../components/Layout"
+import PostList from "../components/PostList"
+import matter from 'gray-matter'
 
-const Index = ({ title, description, ...props }) => {
+const Index = ({ posts, title, description, ...props }) => {
   return (
     <Layout pageTitle={title}>
       <h1>Index</h1>
       <p>{description}</p>
       <main>
-        Posts will go here
+        <PostList posts={posts}/>
       </main>
     </Layout>
   )
@@ -16,10 +18,28 @@ export default Index
 export async function getStaticProps() {
   const configData = await import(`../siteconfig.json`)
 
+  const posts = ((context) => {
+    const keys = context.keys()
+    const values = keys.map(context)
+
+    const data = keys.map((key, index) => {
+      let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3)
+      const value = values[index]
+      const document = matter(value.default)
+      return {
+        frontmatter: document.data,
+        markdownBody: document.content,
+        slug,
+      }
+    })
+    return data
+  })(require.context('../posts', true, /\.md$/))
+
   return {
     props: {
       title: configData.default.title,
-      description: configData.default.description
+      description: configData.default.description,
+      posts
     }
   }
 }
